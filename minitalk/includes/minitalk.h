4d974b69a421210343202e00626fc7e83c0ec118
hateisse <hateisse@student.42.fr>
2022-12-11 15:49:49 +0100

    debut minitlk termine

--- minitalk/includes/minitalk.h
+++ minitalk/includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 09:46:29 by hateisse          #+#    #+#             */
-/*   Updated: 2022/12/08 10:07:38 by hateisse         ###   ########.fr       */
+/*   Updated: 2022/12/11 15:41:09 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include <signal.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+
+typedef struct sigaction t_sigaction;
 
 #endif
--- minitalk/srcs/server/main.c
+++ minitalk/srcs/server/main.c
@@ -6,11 +6,25 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 09:32:01 by hateisse          #+#    #+#             */
-/*   Updated: 2022/12/08 09:55:48 by hateisse         ###   ########.fr       */
+/*   Updated: 2022/12/11 15:41:25 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void    ft_print(int signal)
+{
+    printf("OK %d", signal);
+}
+
+t_sigaction *init_sig_struct(void)
+{
+    t_sigaction *sig;
+
+    sig = malloc(sizeof(*sig));
+    sig->sa_handler = &ft_print;
+    return (sig);
+}
 
 void    display_pid(void)
 {
@@ -20,5 +34,7 @@ void    display_pid(void)
 int main(void)
 {
    display_pid();
+   sigaction(SIGINT, init_sig_struct(), NULL);
+   sleep(60);
    return (0);
 }
